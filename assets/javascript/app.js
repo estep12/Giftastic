e6a81a26467534d99b1f8ceeafe4b8af5025361b
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-20 21:26:36 -0500

    Created function to generate buttons and add them to the DOM

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,34 @@
 
 
 
+var sports = [];
 
+function renderButton(){
+
+    $("#search-button").empty();
+
+    for (i = 0; i < sports.length; i++){
+
+        var sportsB = $("<button>")
+
+        sportsB.addClass("sport");
+        sportsB.attr("data-name", sports[i]);
+        sportsB.text(sports[i]);
+        $("#search-button").append(sportsB);
+    }
+}
+
+$("#addSport").on("click", function(event){
+
+    event.preventDefault();
+
+    var sportStuff = $("#sports-input").val().trim();
+
+    sports.push(sportStuff);
+    
+    renderButton();
+});
+renderButton();
 
 
 
